8948ec21b5fe52cb2fd1d4793962db71abd865af
Mouad Youssef <imouad.you@gmail.com>
2020-02-14 09:52:19 +0100

    Stack

---
+++ DataStructuresCSharp/Stacks/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace DataStructuresCSharp.Stacks
+{
+    class Stack
+    {
+        private int[] items;
+        private int count;
+
+        public Stack(int size)
+        {
+            items = new int[size];
+        }
+
+        public void Push(int item)
+        {
+            if (count == items.Length)
+                throw new StackOverflowException();
+
+            items[count++] = item;
+        }
+
+        public int Pop()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+
+            return items[--count];
+        }
+
+        public int Peek()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+
+            return items[count - 1];
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", items.Where((item, index) => index < count))}]";
+        }
+    }
+}
---
+++ DataStructuresCSharp/Stacks/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresCSharp.Stacks
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack(5);
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            
+            Console.WriteLine(stack);
+        }
+    }
+}
